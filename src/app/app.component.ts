e5d53d8dc5dcc6ece334837bba6adb9c0448ff21
Jeevananthan S <jeevananthan.s@neeyamoworks.com>
2023-07-11 17:25:52 +0530

    Added styling and did validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,30 +4,34 @@ import { form } from './type';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'LOGIN_TeamProject';
-  form:form={} as form;
+  regex: string = '[a-zA-Z. ]*';
+  form: form = {} as form;
   // public password:string|number=this.form.password;
-isSame:boolean=false;
-  check(){
-    if(this.form.password===this.form.confirmPassword){
-      this.isSame=true;
-    }
+  isSame: boolean = false;
+  check() {
+    this.isSame =
+      this.form.password === this.form.confirmPassword ? true : false;
   }
+  usernameValidate() {}
+
   loginData: form[] = [];
-  createUser(){
+  createUser() {
     this.loginData.push({ ...this.form });
+    console.log(this.loginData);
+    
     this.resetForm();
   }
-  resetForm()
-  {
+
+  resetForm() {
     this.form = {
-      username:'',
-      password:'',
-      confirmPassword:'',
-      
-  };this.isSame=false;
+      username: '',
+      password: '',
+      confirmPassword: '',
+    };
+    this.isSame = false;
   }
 }
